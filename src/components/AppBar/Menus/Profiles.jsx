2ccb6c15fe43879f20df81e04850e1ffa72fcd57
truong <truonglemanh295@gmail.com>
2025-07-06 17:36:39 +0700

    Appbar

---
+++ src/components/AppBar/Menus/Profiles.jsx
@@ -0,0 +1,94 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import Divider from '@mui/material/Divider'
+import Avatar from '@mui/material/Avatar'
+import Tooltip from '@mui/material/Tooltip'
+import IconButton from '@mui/material/IconButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+
+// ✅ Thêm các icon còn thiếu
+import PersonAdd from '@mui/icons-material/PersonAdd'
+import Settings from '@mui/icons-material/Settings'
+import Logout from '@mui/icons-material/Logout'
+
+function Profiles() {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const open = Boolean(anchorEl)
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  return (
+    <Box>
+      <Tooltip title="Account settings">
+        <IconButton
+          id="basic-button-profiles"
+          onClick={handleClick}
+          size="small"
+          sx={{ p: 0 }}
+          aria-controls={open ? 'basic-menu-profiles' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+        >
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            alt="truong0323"
+            src="https://tse4.mm.bing.net/th/id/OIP.mAf5lulp0edjG9zvEsXKXQHaEN?pid=Api&P=0&h=180"
+          />
+        </IconButton>
+      </Tooltip>
+      <Menu
+        id="basic-menu-profiles"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        slotProps={{
+          list: {
+            'aria-labelledby': 'basic-button-profiles',
+          },
+        }}
+      >
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Avatar sx={{ width: 28,mr:2, height: 28 }} />
+          </ListItemIcon>
+          Profile
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Avatar sx={{ width: 28,mr:2, height: 28 }} />
+          </ListItemIcon>
+          My account
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <PersonAdd fontSize="small" />
+          </ListItemIcon>
+          Add another account
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </Box>
+  )
+}
+
+export default Profiles
--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,18 +1,71 @@
 import Box from '@mui/material/Box'
-import ModeSelect from '../../components/ModeSelect'
-
+import ModeSelect from '~/components/ModeSelect'
+import AppsIcon from '@mui/icons-material/Apps'
+import { ReactComponent as TrelloIcon } from '~/assets/trello.svg'
+import SvgIcon from '@mui/material/SvgIcon'
+import Typography from '@mui/material/Typography'
+import Workspaces from './Menus/Workspaces'
+import Recent from './Menus/Recent'
+import Starred from './Menus/Starred'
+import Templates from './Menus/Templates'
+import Profiles from './Menus/Profiles'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
+import Badge from '@mui/material/Badge'
+import Tooltip from '@mui/material/Tooltip'
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
 function AppBar() {
   return (
     <Box
+      px={2}
       sx={(theme) => ({
-        backgroundColor: 'primary.light',
         width: '100%',
         height: theme.trello.appbarHeight,
         display: 'flex',
-        alignItems: 'center'
+        alignItems: 'center',
+        backgroundColor: theme.palette.background.default,
+        color: theme.palette.text.primary,
+        justifyContent: 'space-between'
       })}
     >
-      <ModeSelect />
+      <Box sx={{ display:'flex', alignItems: 'center', gap:2 }}>
+        <AppsIcon sx={{ color: 'primary.main' }}/>
+        <Box sx={{ display:'flex', alignItems: 'center', gap:0.5 }}>
+          <SvgIcon component={TrelloIcon} inheritViewBox sx={{ color:'primary.main'}} />
+          <Typography variant="span" sx={{fontSize: '1.2rem', fontWeight: 'bold', color: 'primary.main' }} >
+            Trello
+          </Typography>
+          <Workspaces/>
+          <Recent/>
+          <Starred/>
+          <Templates/>
+
+          <Button variant="outlined">Create</Button>
+        </Box>
+
+      </Box>
+      <Box sx={{ display:'flex', alignItems: 'center', gap:0.5 }}>
+        <TextField
+          id="standard-search"
+          label="Search..."
+          type="search"
+          variant="outlined"
+          size="small"
+        />
+        <ModeSelect />
+        <Tooltip title="Notification" >
+          <Badge color="secondary" variant="dot" sx={{ cursor:'pointer' }}>
+            <NotificationsNoneIcon sx={{ color: 'primary.main' }}/>
+          </Badge>
+        </Tooltip>
+
+        <Tooltip title="help" >
+          <HelpOutlineIcon sx={{ cursor:'pointer', color: 'primary.main' }}/>
+        </Tooltip>
+
+        <Profiles/>
+      </Box>
     </Box>
   )
 }
